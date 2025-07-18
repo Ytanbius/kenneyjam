933bd7f1ec91c11aa36dc93c436ba1c26b1e370d
jeffin-man <jeffersonjunior1b@gmail.com>
2025-07-18 15:24:41 -0300

    menu funcional

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    // Update is called once per frame
+    public void Loadescena()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    
+    
+    
+    
+    
+}
